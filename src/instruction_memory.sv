a110125d07763e923e4f052ac0780382b34db2d9
Matth-M <matthieu.matsima@gmail.com>
2025-04-02 12:35:25 +0200

    Initialize instruction memory with memory file

--- src/instruction_memory.sv
+++ src/instruction_memory.sv
@@ -1,10 +1,11 @@
 `default_nettype none
 module instruction_memory #(
-    parameter integer WORDS = 64
+    parameter integer WORDS = 64,
+    parameter str MEM_INIT_FILENAME = ""
 ) (
     input logic [31:0] read_address,
     input logic reset_n,
-	input logic clk,
+    input logic clk,
     output logic [31:0] instruction
 );
 
@@ -12,6 +13,11 @@ module instruction_memory #(
   logic [31:0] memory[WORDS-1];  // Memory containing WORDS elements of 32-bit
 
   assign instruction = memory[read_address];
+  initial begin
+    if (MEM_INIT_FILENAME != "") begin
+      $readmemh(MEM_INIT_FILENAME, memory);
+    end
+  end
 
   always_ff @(posedge clk) begin
     if (reset_n == 0) begin
