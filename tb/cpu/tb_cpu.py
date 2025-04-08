281e74c5274bdad71d511d0686e95abf830d177e
Matth-M <matthieu.matsima@gmail.com>
2025-04-08 13:33:58 +0200

    Add functions to read hex file and convert binary str to hex

--- tb/cpu/tb_cpu.py
+++ tb/cpu/tb_cpu.py
@@ -1,7 +1,20 @@
 from decimal import Decimal
+from pathlib import Path
 
 import cocotb
 from cocotb.triggers import Timer
+
+def bin_to_hex(bin) -> str:
+    return hex(int(str(bin), 2)).upper()
+
+
+def read_hex_file(file: Path) -> list[int]:
+    hex = []
+    with file.open("r") as f:
+        content = f.readlines()
+        for line in content:
+            hex.append(int(line.split("//")[0].strip().upper(), base=16))
+    return hex
 
 
 @cocotb.test
