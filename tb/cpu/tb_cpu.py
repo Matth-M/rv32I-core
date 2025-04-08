49c85ff37c54bd895b78f9a528eba73b3d00b8a2
Matth-M <matthieu.matsima@gmail.com>
2025-04-08 13:32:04 +0200

    Add cpu_reset test

--- tb/cpu/tb_cpu.py
+++ tb/cpu/tb_cpu.py
@@ -2,7 +2,9 @@ from decimal import Decimal
 from pathlib import Path
 
 import cocotb
-from cocotb.triggers import Timer
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, Timer
+
 
 def bin_to_hex(bin) -> str:
     return hex(int(str(bin), 2)).upper()
@@ -17,7 +19,31 @@ def read_hex_file(file: Path) -> list[int]:
     return hex
 
 
+@cocotb.coroutine
+async def cpu_reset(dut):
+    dut.reset_n.value = 0
+    await RisingEdge(dut.clk)
+    dut.reset_n.value = 1
+    await RisingEdge(dut.clk)
+
+
 @cocotb.test
-async def test(dut):
-    await Timer(Decimal(1),units="ns")
-    dut._log.info(f"instruction_memory: {dut.instr_mem.memory.value[0]}")
+async def test_cpu_init(dut):
+    cocotb.start_soon(Clock(dut.clk, 10, units="ns").start())
+    await cpu_reset(dut)
+
+    assert dut.pc.value == 0
+
+    instruction_memory = read_hex_file(Path("./instruction_memory.hex"))
+
+    # Check that instruction are still in memory
+    for i in range(len(instruction_memory)):
+        assert dut.instr_mem.memory[i].value == instruction_memory[i]
+
+    # Test that instruction given by PC during executions are the ones expected
+    # Only test for a few instructions, branching occurs after the start of the program
+    maximum_instruction_to_test = 4
+    for i in range(min(maximum_instruction_to_test, len(instruction_memory))):
+        expected_instruction = instruction_memory[i]
+        assert dut.instruction.value == expected_instruction
+        await RisingEdge(dut.clk)
