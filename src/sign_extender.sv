b9d17e1ef309a8e22a81ef27f0a7c42895ee5067
Matth-M <matthieu.matsima@gmail.com>
2025-04-09 16:40:20 +0200

    Use constants declared in pkg to set imm_ext value based on instruction and imm_src

--- src/sign_extender.sv
+++ src/sign_extender.sv
@@ -1,10 +1,15 @@
 `default_nettype none
 module sign_extender (
-    input wire imm_src,
-    input wire [31:0] instruction,
+    input  wire [ 1:0] imm_src,
+    input  wire [31:0] instruction,
     output wire [31:0] imm_ext
 );
-  assign imm_ext = imm_src ?
-  {{20{instruction[31]}}, instruction[31:25], instruction[11:7]} : // S-Type
-  {{20{instruction[31]}}, instruction[31:20]};  // I-Type
+
+  always_comb begin
+    case (imm_src)
+      IMM_SRC_I_TYPE: imm_ext = {{20{instruction[31]}}, instruction[31:20]};
+      IMM_SRC_S_TYPE: imm_ext = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
+      default: imm_ext = 32'b0;
+    endcase
+  end
 endmodule
