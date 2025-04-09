3995c051d51b77d56cd3c7187dfef37e74226814
Matth-M <matthieu.matsima@gmail.com>
2025-04-09 11:39:49 +0200

    format

--- src/program_counter.sv
+++ src/program_counter.sv
@@ -1,17 +1,17 @@
 `default_nettype none
 module program_counter (
-	input logic [31:0] pc_next,
-	input logic clk,
-	input logic reset_n,
-	output logic [31:0] pc
+    input logic [31:0] pc_next,
+    input logic clk,
+    input logic reset_n,
+    output logic [31:0] pc
 );
 
   always_ff @(posedge clk) begin
-	  if (reset_n == 0) begin
-		  pc <= 32'b0;
-	  end else begin
-		  pc <= pc_next;
-	  end
+    if (reset_n == 0) begin
+      pc <= 32'b0;
+    end else begin
+      pc <= pc_next;
+    end
   end
 
 endmodule
