bad8f14b39e587e7e1043c2610376842e23ae0c6
Matth-M <matthieu.matsima@gmail.com>
2025-04-15 16:46:56 +0200

    Add function to print regfile content

--- tb/utils.py
+++ tb/utils.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+
 def bin_to_hex(bin) -> str:
     return hex(int(str(bin), 2)).upper().split("X")[1].zfill(8)
 
@@ -17,8 +18,18 @@ def bin_array_to_hex(bin_array: list) -> list:
     return list(map(bin_to_hex, bin_array))
 
 
+def read_regfile(registers: list) -> str:
+    registers_hex = bin_array_to_hex(registers)
+    s = "["
+    for i in range(len(registers_hex) - 1):
+        s += f"{i}: {registers_hex[i]}, "
+    s += f"{len(registers_hex)-1}: {registers_hex[-1]}]\n"
+    return s
+
+
 def truncate_32_bits(n: int) -> int:
     return n & 0xFFFFFFFF
+
 
 if __name__ == "__main__":
     pass
